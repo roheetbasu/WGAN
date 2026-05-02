733dcf2c3b8cf271f5b0fb19f6dfed22b2a4cc1b
Roheet <roheetbasu@gmail.com>
2026-05-02 20:05:39 +0545

    added data.py

---
+++ data.py
@@ -0,0 +1,11 @@
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+from config import BATCH_SIZE, IMG_SHAPE
+
+def get_dataloader(batch_size: int = BATCH_SIZE, data_root: str = "./data"):
+    
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.5,), std=(0.5,)),
+    ])
