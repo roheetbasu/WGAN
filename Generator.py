9721ee0ea05ea7b290dff5a6d6cb093f884d6edd
Roheet <roheetbasu@gmail.com>
2026-05-04 21:10:25 +0545

    completed init function

--- Generator.py
+++ Generator.py
@@ -13,9 +13,32 @@ class Generator(nn.Module):
             nn.LeakyReLU(0.2, inplace=True)
         )
 
+        # 4 x 4 -> 8 x 8
         self.up1 = nn.Sequential(
             nn.Upsample(scale_factor=2, mode="nearest"),
-            nn.Conv2d(256, 128, kernel_size=3, padding=1, bias=False)
+            nn.Conv2d(256, 128, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True)
         )
+        # 8 x 8 -> 16 x 16
+        self.up2 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode="nearest"),
+            nn.Conv2d(128, 64, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        
+        # 16 x 16 -> 32 x 32
+        self.up3 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode="nearest"),
+            nn.Conv2d(64, 1, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(1),
+            nn.Tanh()
+        )
+        
+        self.crop = nn.ZeroPad2d(-2)
+        
+    
+    def forward(self, z:Tensor):
+        x = self.project(z)
+        x = x.view(-1, 256, 4 , 4)
